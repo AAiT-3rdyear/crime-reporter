de82ac1ed9a1114eba7a7fd9e4a7678d4de1f7be
Gizaw <gizawag123@gmail.com>
2022-05-08 21:22:48 +0300

    adding station model

---
+++ backEnd/models/policeStationModel.js
@@ -0,0 +1,37 @@
+
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema
+
+// const policeSchema = new mongoose.Schema({
+//     stationName : {
+//         type: String, 
+//         required:true,},
+
+//     imageURL : {
+//         type:String,
+//         default:'default.jpg'},
+
+//     email: {
+//         type:String,
+//         required:true,
+//         unique:true,},
+
+//     phone_no:  {
+//         type:String,
+//         required:true},
+//     password : {
+//         type:String, 
+//         required:true,},
+        
+//     role: {
+//         type:String,
+//          default:"Police"},
+
+//     location: {
+//          type: String,
+//          required: true},
+
+// });
+
+// const PoliceStation = mongoose.model('PoliceStation', policeSchema)
+// module.exports = PoliceStation;
