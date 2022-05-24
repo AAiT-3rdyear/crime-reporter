6fcae29e4e0d98b07186a14f489ee4bb7f2dac71
Gizaw <gizawag123@gmail.com>
2022-05-24 22:29:04 +0300

    updating pages

--- front-end/test/widget_test.dart
+++ front-end/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:crime_reporter/main.dart';
+// import 'package:crime_reporter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
