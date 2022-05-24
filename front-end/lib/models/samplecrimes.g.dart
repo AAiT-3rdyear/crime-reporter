12e7b69aaceb9248ce7ad607b46b6401cc0ed841
abi26anamo <abianamo282@gmail.com>
2022-05-24 22:14:26 +0300

    added  to models

---
+++ front-end/lib/models/samplecrimes.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'samplecrimes.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SampleCrime _$SampleCrimeFromJson(Map<String, dynamic> json) => SampleCrime(
+      image: json['imageURL'] as String,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$SampleCrimeToJson(SampleCrime instance) =>
+    <String, dynamic>{
+      'imageURL': instance.image,
+      'name': instance.name,
+    };
