ddbadcd17c4241bfeca4cf29809a8099b90c0394
abi26anamo <abianamo282@gmail.com>
2022-06-05 01:39:41 +0300

    added componenets.dart

---
+++ front-end/lib/presentation/components/componenets.dart
@@ -0,0 +1,5 @@
+export  'package:crime_reporter/components/crimeCard.dart';
+export  'package:crime_reporter/components/reportedCard.dart';
+export  'package:crime_reporter/presentation/components/bottumNavBar.dart';
+export  'package:crime_reporter/presentation/components/cliper.dart';
+export  'package:crime_reporter/components/myReportsCard.dart';
