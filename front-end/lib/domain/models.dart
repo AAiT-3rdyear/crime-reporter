8b42a13d5507d2bdc1b4a1b370d8d7f39e32be7f
abi26anamo <abianamo282@gmail.com>
2022-06-05 01:20:52 +0300

    added models.dart

---
+++ front-end/lib/domain/models.dart
@@ -0,0 +1,5 @@
+export  'package:crime_reporter/domain/crime/report.dart';
+export  'package:crime_reporter/domain/User/user.dart';
+export  'package:crime_reporter/domain/Common/welcomeScreenArguments.dart';
+export  'package:crime_reporter/domain/Crime/samplecrimes.dart';
+
