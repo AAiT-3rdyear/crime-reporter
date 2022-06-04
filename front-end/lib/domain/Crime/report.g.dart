b57fc26b88ed66e964d91251bbfe96df948b6506
Gizaw <gizawag123@gmail.com>
2022-06-05 01:16:03 +0300

    adding crime seralizer

---
+++ front-end/lib/domain/Crime/report.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'report.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Report _$ReportFromJson(Map<String, dynamic> json) => Report(
+      crimeId: json['_id'] as String,
+      title: json['title'] as String,
+      location: json['location'] as String,
+      time: json['time'] as String,
+      description: json['description'] as String,
+      imageURL: json['imageURL'] as String,
+      reportTo: json['report_to'] as String,
+      status: json['status'] as String,
+
+      reporter: User.fromJson(json['reporter'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$ReportToJson(Report instance) => <String, dynamic>{
+      '_id': instance.crimeId,
+      'title': instance.title,
+      'location': instance.location,
+      'time': instance.time,
+      'description': instance.description,
+      'imageURL': instance.imageURL,
+      'report_to': instance.reportTo,
+       'status':instance.status,
+      'reporter': instance.reporter.toJson(),
+    };
