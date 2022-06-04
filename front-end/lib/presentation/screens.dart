8667565ecc3e3e6ea3936086af3942d83523b074
Gizaw <gizawag123@gmail.com>
2022-06-05 01:45:00 +0300

    adding screens file

---
+++ front-end/lib/presentation/screens.dart
@@ -0,0 +1,19 @@
+export  'package:crime_reporter/presentation/auth/profileScreen.dart';
+export 'package:crime_reporter/presentation/user/reportDetail.dart';
+export  'package:crime_reporter/presentation/auth/login.dart';
+export  'package:crime_reporter/presentation/auth/signup.dart';
+export  'package:crime_reporter/presentation/user/welcomeScreen.dart';
+export  'package:crime_reporter/presentation/splash/splashScreen.dart';
+export  'package:crime_reporter/presentation/auth/editProfileScreen.dart';
+export  'package:crime_reporter/presentation/police/ongoingCrimesScreen.dart';
+export  'package:crime_reporter/presentation/splash/splashScreen.dart';
+export  'package:crime_reporter/presentation/user/userViewDetail.dart';
+export  'package:crime_reporter/presentation/police/solvedCrimesScreen.dart';
+export  'package:crime_reporter/presentation/auth/profileScreen.dart';
+export  'package:crime_reporter/presentation/police/policeScreen.dart';
+export  'package:crime_reporter/presentation/user/myReportsScreen.dart';
+export  'package:crime_reporter/presentation/police/policeViewDetail.dart';
+export  'package:crime_reporter/presentation/common/aboutScreen.dart';
+
+
+
