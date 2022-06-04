abae8f5df689c7874845b4d7c9425ac80fdd1084
biruk <birukanley2022@gmail.com>
2022-06-05 01:44:00 +0300

    adding solved crime screen

---
+++ front-end/lib/presentation/police/solvedCrimesScreen.dart
@@ -0,0 +1,38 @@
+import 'package:crime_reporter/components/reportedCard.dart';
+import 'package:flutter/material.dart';
+
+class SolvedCrime extends StatefulWidget {
+  SolvedCrime({Key? key, this.solved}) : super(key: key);
+  dynamic solved;
+  @override
+  State<SolvedCrime> createState() => _SolvedCrimeState();
+}
+
+class _SolvedCrimeState extends State<SolvedCrime> {
+  @override
+  Widget build(BuildContext context) {
+    return widget.solved.length == 0
+        ? Container(
+            padding: const EdgeInsets.all(20),
+            child: const Text(
+              "no solved crimes list",
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+            ))
+        : Expanded(
+            flex: 4,
+            child: ListView.builder(
+                itemCount: (widget.solved).length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ReportedCard(
+                    crimeId: widget.solved[index]['_id'],
+                    title: widget.solved[index]['status'],
+                    discription: widget.solved[index]["description"],
+                    location: widget.solved[index]["location"],
+                    time: widget.solved[index]["time"],
+                    path: widget.solved[index]["imageURL"],
+                    reportto: widget.solved[index]['report_to'],
+                  );
+                }),
+          );
+  }
+}
