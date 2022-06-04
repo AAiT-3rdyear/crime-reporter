db6386c1ac317024ebe4488b0905bc93209796fc
abi26anamo <abianamo282@gmail.com>
2022-06-05 01:47:28 +0300

    added myReportsScreen.dart

---
+++ front-end/lib/presentation/user/myReportsScreen.dart
@@ -0,0 +1,28 @@
+import 'package:crime_reporter/components/myReportsCard.dart';
+import 'package:flutter/material.dart';
+
+class MyReports extends StatelessWidget {
+  MyReports({
+    Key? key,
+    this.myreports,
+    this.user,
+  }) : super(key: key);
+  dynamic myreports;
+  dynamic user;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Column(children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: myreports.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return CrimeCard2(myreports: myreports, index: index);
+                }),
+          ),
+        ]),
+      ),
+    );
+  }
+}
