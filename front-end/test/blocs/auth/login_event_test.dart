b60c489b4e1177dd66f825440b469d90a825541a
Gizaw <gizawag123@gmail.com>
2022-06-05 03:12:16 +0300

    auth event test

---
+++ front-end/test/blocs/auth/login_event_test.dart
@@ -0,0 +1,13 @@
+
+// import 'package:flutter_test/flutter_test.dart';
+// import '../../../lib/bloc/authBloc/auth_bloc.dart';
+
+// void main() {
+//   group('LoginEvent', () {
+//     group('LoginSubmitted', () {
+//       test('supports value comparisons', () {
+//         expect(Login(body: ), LogingIn());
+//       });
+//     });
+//   });
+// }
