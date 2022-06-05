35a9218f1f4bfbe8c94c2775831e41f8e75e6f3e
biruk <birukanley2022@gmail.com>
2022-06-05 03:14:56 +0300

    police bloc test

---
+++ front-end/test/blocs/police/policbloc_test.dart
@@ -0,0 +1,74 @@
+import 'package:crime_reporter/models/models.dart';
+import 'package:crime_reporter/repositories/police_repositories.dart';
+import 'package:crime_reporter/repositories/user_repositories.dart';
+import 'package:flutter/cupertino.dart';
+import '../../../lib/bloc/policeBloc/policebloc_bloc.dart';
+import 'package:bloc_test/bloc_test.dart';
+import '../../../lib/bloc/authBloc/auth_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:http/http.dart';
+
+class MockAuthenticationRepository extends Mock implements PoliceRepository {}
+
+void main() {
+  late PoliceRepository authenticationRepository;
+
+  setUp(() {
+    authenticationRepository = MockAuthenticationRepository();
+  });
+
+  test('initial state is userbloc', () {
+    final userblocBloc = PoliceblocBloc();
+    expect(userblocBloc.state, PoliceblocBloc());
+  });
+
+  group('Bloc test', () {
+    blocTest<PoliceblocBloc, PoliceblocState>(
+      'emits sample found for crime',
+      setUp: () {
+        when(
+          () => authenticationRepository.getReportedCrimes(Path()),
+        ).thenAnswer((_) async {});
+      },
+      build: () => PoliceblocBloc(),
+      act: (bloc) {
+        bloc..add(GetOngoing(Path()));
+      },
+      expect: () => GetReportedSucessful(),
+    );
+
+    blocTest<PoliceblocBloc, PoliceblocState>(
+      'select station',
+      setUp: () {
+        when(
+          () => authenticationRepository.getSolvedCrimes('user'),
+        ).thenAnswer((_) async {
+        });
+      },
+      build: () => PoliceblocBloc(),
+      act: (bloc) {
+        
+        PoliceRepository myrepo = PoliceRepository();
+        dynamic solved = myrepo.getSolvedCrimes('user');
+        bloc.add(GetSolved(solved));
+       
+      },
+      expect: () => GetSolvedSucessful(),
+    );
+
+    blocTest<PoliceblocBloc, PoliceblocState>(
+      'select station',
+      setUp: () {
+        when(
+          () => authenticationRepository.getOngoingCrimes(Path()),
+        ).thenAnswer((_) async {});
+      },
+      build: () => PoliceblocBloc(),
+      act: (bloc) {
+        bloc..add(GetOngoing(Path()));
+      },
+      expect: () => GetOngoingSucessful(),
+    );
+  });
+}
