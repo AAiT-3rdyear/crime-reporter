2a3a25812abd9e6a12d33006c9a95bd99af262b3
Gizaw <gizawag123@gmail.com>
2022-06-05 03:12:32 +0300

    auth state test

---
+++ front-end/test/blocs/auth/login_state_test.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import '../../../lib/bloc/authBloc/auth_bloc.dart';
+
+void main() {
+  group('LoginState', () {
+    test('supports value comparisons', () {
+      expect(AuthInitial(), AuthInitial());
+    });
+
+    
+  });
+}
